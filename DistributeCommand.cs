90ae9070d349e2fbba05162c6e32e8b9b383efdd
Marvin Fuchs <info@marvin-fuchs.de>
2023-02-13 15:06:56 +0100

    Switch to .net core and switch to spectre.console.cli

---
+++ DistributeCommand.cs
@@ -0,0 +1,205 @@
+﻿// <copyright file="DistributeCommand.cs" company="Marvin Fuchs">
+// Copyright (c) Marvin Fuchs. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Directory = System.IO.Directory;
+
+namespace Distribute
+{
+    /// <summary>
+    /// Class containing the distribute command that is responsible for distributing the images from the source directory.
+    /// </summary>
+    internal class DistributeCommand : Command<Options>
+    {
+        /// <inheritdoc/>
+        public override int Execute([NotNull] CommandContext context, [NotNull] Options options)
+        {
+            AnsiConsole.Foreground = Color.Green;
+
+            Queue<(string, DateTime)> images = new ();
+
+            // Indexing images and load metadata
+            AnsiConsole.Progress()
+                .Columns(new ProgressColumn[]
+                {
+                    new ProgressBarColumn(),
+                    new PercentageColumn(),
+                    new RemainingTimeColumn(),
+                    new ElapsedTimeColumn(),
+                    new TaskDescriptionColumn() { Alignment = Justify.Left },
+                })
+                .AutoClear(true)
+                .HideCompleted(true)
+                .Start(ctx => images = new (GetImagePaths(options.From, 0, options.Depth, ctx)));
+
+            AnsiConsole.Write(new Rule($"Metadata loaded. [yellow]{images.Count}[/] files to distribute!")
+            {
+                Style = Style.Parse("green"),
+            });
+
+            if (images.Count == 0)
+            {
+                AnsiConsole.MarkupLine("No images found. Finished!");
+                return 0;
+            }
+
+            // Start copy images
+            var duplicates = CopyImages(images, options.To, options.Structure);
+
+            if (duplicates.Count == 0)
+            {
+                AnsiConsole.MarkupLine("No duplicates. Finished!");
+                return 0;
+            }
+
+            // Ask what should happen with duplicates.
+            AnsiConsole.Write(new Rule($"[green]There are [yellow]{duplicates.Count}[/] duplicate files![/]")
+            {
+                Style = Style.Parse("green"),
+            });
+
+            AnsiConsole.WriteLine();
+
+            var duplicatesOption = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("[yellow]How would you like to proceed with the duplicate files?[/]")
+                .AddChoices(new[]
+                {
+                    "Skip",
+                    "Overwrite",
+                    "Add \"_copy\" to filename",
+                }));
+
+            switch (duplicatesOption)
+            {
+                case "Skip":
+                    return 0;
+                case "Overwrite":
+                    CopyImages(duplicates, options.To, options.Structure, true);
+                    return 0;
+                default:
+                    CopyImages(duplicates, options.To, options.Structure, false, "_copy");
+                    return 0;
+            }
+        }
+
+        private static Queue<(string, DateTime)> CopyImages(Queue<(string, DateTime)> images, string to, string structure, bool overwrite = false, string? filenameAppend = null)
+        {
+            var duplicates = new Queue<(string, DateTime)>();
+
+            AnsiConsole.Progress()
+                .Columns(new ProgressColumn[]
+                {
+                    new TaskDescriptionColumn(),
+                    new ProgressBarColumn(),
+                    new PercentageColumn(),
+                    new RemainingTimeColumn(),
+                    new ElapsedTimeColumn(),
+                })
+                .Start(ctx =>
+                {
+                    var task = ctx.AddTask("Copying files... ", maxValue: images.Count);
+
+                    do
+                    {
+                        var image = images.Dequeue();
+                        var directory = to + @"\" + image.Item2.ToString(structure);
+                        var path = directory + Path.GetFileName(image.Item1);
+                        if (filenameAppend != null)
+                        {
+                            var split = path.Split('.');
+                            split[0] += filenameAppend;
+                            path = split[0] + "." + split[1];
+                        }
+
+                        if (!Directory.Exists(directory))
+                        {
+                            Directory.CreateDirectory(directory);
+                        }
+
+                        try
+                        {
+                            File.Copy(image.Item1, path, overwrite);
+                        }
+                        catch (IOException)
+                        {
+                            duplicates.Enqueue(image);
+                        }
+
+                        task.Increment(1);
+                    }
+                    while (images.Count > 0);
+                });
+
+            return duplicates;
+        }
+
+        private static List<(string, DateTime)> GetImagePaths(string directory, uint depth, int maxDepth, ProgressContext ctx)
+        {
+            string[] files = Directory.GetFiles(directory);
+
+            var task = ctx.AddTask($"[grey][white]Loading images...         [/] {directory}[/]", maxValue: files.Length + 1);
+
+            List<(string, DateTime)> images = new (files.Length);
+
+            foreach (string image in files)
+            {
+                ExifSubIfdDirectory metadata;
+
+                try
+                {
+                    metadata = ImageMetadataReader
+                        .ReadMetadata(image)
+                        .OfType<ExifSubIfdDirectory>()
+                        .FirstOrDefault() !;
+                }
+                catch (ImageProcessingException)
+                {
+                    task.Increment(1);
+                    continue;
+                }
+
+                task.Increment(1);
+
+                if (metadata == null)
+                {
+                    continue;
+                }
+
+                if (metadata.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out DateTime datetime))
+                {
+                    images.Add((image, datetime));
+                }
+            }
+
+            var subdirectories = Directory.GetDirectories(directory);
+
+            if (subdirectories.Length == 0)
+            {
+                task.Value = task.MaxValue;
+                return images;
+            }
+
+            task.Description = $"[grey][white]Loading sub directories...[/] {directory}[/]";
+            task.Value = 0;
+            task.MaxValue = subdirectories.Length;
+
+            if (depth < maxDepth)
+            {
+                foreach (string subdirectory in subdirectories)
+                {
+                    images.AddRange(GetImagePaths(subdirectory, depth + 1, maxDepth, ctx));
+
+                    task.Increment(1);
+                }
+            }
+
+            return images;
+        }
+    }
+}
--- Options.cs
+++ Options.cs
@@ -1,22 +1,107 @@
-﻿namespace Distribute
+﻿// <copyright file="Options.cs" company="Marvin Fuchs">
+// Copyright (c) Marvin Fuchs. All rights reserved.
+// </copyright>
+
+namespace Distribute
 {
-    using CommandLine;
+    using System.ComponentModel;
+    using Spectre.Console;
+    using Spectre.Console.Cli;
 
-    internal class Options
+    /// <summary>
+    /// Class containing the command line options.
+    /// </summary>
+    public class Options : CommandSettings
     {
-        [Option('f', "from", Group = "Paths", HelpText = "The path to the files that sould be distributed or empty for the current path.")]
-        public string From { get; set; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Options"/> class.
+        /// </summary>
+        public Options(
+            string from,
+            string to,
+            string structure,
+            int depth,
+            bool remove)
+        {
+            this.From = from == "The current directory." ? Directory.GetCurrentDirectory() : from;
+            this.To = to == "The current directory." ? Directory.GetCurrentDirectory() : to;
+            this.Structure = structure ?? @"yyyy\\MM\\";
+            this.Depth = depth;
+            this.Remove = remove;
+        }
 
-        [Option('t', "to", Group = "Paths", HelpText = "The path where the files should be distributed to or empty for the current path.")]
-        public string To { get; set; }
+        /// <summary>
+        /// Gets the source directory the files should be distributed from.
+        /// </summary>
+        [Description("The source directory the files should be distributed from.")]
+        [CommandOption("-f|--from")]
+        [DefaultValue("The current directory.")]
+        public string From { get; private set;}
 
-        [Option('s', "structure", HelpText = "The folder structure the files should be sorted into.", Default = "yyyy\\\\MM\\\\")]
-        public string Structure { get; set; }
+        /// <summary>
+        /// Gets the source directory the files should be distributed from.
+        /// </summary>
+        [Description("The directory the files should be distributed to.")]
+        [CommandOption("-t|--to")]
+        [DefaultValue("The current directory.")]
+        public string To { get; private set; }
 
-        [Option('d', "depth", HelpText = "The search depth.", Default = 5)]
-        public int Depth { get; set; }
+        /// <summary>
+        /// Gets the folder structure the files should be sorted into.
+        /// </summary>
+        /// <seealso href="https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings">Date and time format strings.</seealso>
+        [Description("The folder structure the files should be sorted into.\nSee https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings for more information.")]
+        [CommandOption("-s|--structure")]
+        [DefaultValue(@"yyyy\\MM\\")]
+        public string Structure { get; }
 
-        [Option('r', "remove", HelpText = "Wether copied images should be deleted in the source.")]
-        public bool Remove { get; set; }
+        /// <summary>
+        /// Gets the maximum search depth for files in the <see cref="Distribute.Options.From"/> directory.
+        /// </summary>
+        [Description("The maximum search depth for files in the source directory.")]
+        [CommandOption("-d|--depth")]
+        [DefaultValue(5)]
+        public int Depth { get; } = 5;
+
+        /// <summary>
+        /// Gets a value indicating whether the distributed and copied files should be deleted in the source directory.
+        /// </summary>
+        [Description("Whether the distributed and copied files should be deleted in the source directory.")]
+        [CommandOption("-r|--remove")]
+        public bool Remove { get; }
+
+        /// <inheritdoc/>
+        public override ValidationResult Validate()
+        {
+            if (this.Depth < 1)
+            {
+                return ValidationResult.Error("Option [-d|--depth] must be at least 1.");
+            }
+
+            if (this.From == Directory.GetCurrentDirectory() && this.To == Directory.GetCurrentDirectory())
+            {
+                return ValidationResult.Error("Either option [-f|--from] or [-t|--to] has to be specified.");
+            }
+
+            try
+            {
+                this.From = Path.GetFullPath(this.From);
+            }
+            catch
+            {
+                return ValidationResult.Error("The option [-f|--from] is an invalid path.");
+            }
+
+            try
+            {
+                this.To = Path.GetFullPath(this.To);
+            }
+            catch
+            {
+                return ValidationResult.Error("The option [-t|--to] is an invalid path.");
+            }
+
+            return ValidationResult.Success();
+        }
     }
 }
