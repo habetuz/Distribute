83e2fd83e7975fbeed2bb6e51f0e99439e476e5c
Marvin Fuchs <marfinn3@gmail.com>
2024-12-22 20:21:21 +0100

    Format

--- DistributeCommand.cs
+++ DistributeCommand.cs
@@ -5,149 +5,90 @@ using Spectre.Console;
 using Spectre.Console.Cli;
 using Directory = System.IO.Directory;
 
-namespace Distribute
+namespace Distribute;
+
+public class DistributeCommand : Command<Options>
 {
-  internal class DistributeCommand : Command<Options>
+  public override int Execute([NotNull] CommandContext context, [NotNull] Options options)
   {
-    public override int Execute([NotNull] CommandContext context, [NotNull] Options options)
+    AnsiConsole.Foreground = Color.Green;
+
+    Queue<(string, DateTime)> images = new();
+
+    // Indexing images and load metadata
+    AnsiConsole
+      .Progress()
+      .Columns(
+        [
+          new ProgressBarColumn(),
+          new PercentageColumn(),
+          new RemainingTimeColumn(),
+          new ElapsedTimeColumn(),
+          new TaskDescriptionColumn() { Alignment = Justify.Left },
+        ]
+      )
+      .AutoClear(true)
+      .HideCompleted(true)
+      .Start(ctx => images = new(GetImagePaths(options.From, 0, options.Depth, ctx)));
+
+    AnsiConsole.Write(
+      new Rule($"Metadata loaded. [yellow]{images.Count}[/] files to distribute!")
+      {
+        Style = Style.Parse("green"),
+      }
+    );
+
+    if (images.Count == 0)
     {
-      AnsiConsole.Foreground = Color.Green;
+      AnsiConsole.MarkupLine("No images found. Finished!");
+      return 0;
+    }
 
-      Queue<(string, DateTime)> images = new();
+    // Cast (path, date) to (path) queue.
+    var imagePaths = new Queue<string>(
+      images.Cast<(string, DateTime)>().Select(image => image.Item1)
+    );
 
-      // Indexing images and load metadata
-      AnsiConsole
-        .Progress()
-        .Columns(
-          [
-            new ProgressBarColumn(),
-            new PercentageColumn(),
-            new RemainingTimeColumn(),
-            new ElapsedTimeColumn(),
-            new TaskDescriptionColumn() { Alignment = Justify.Left },
-          ]
-        )
-        .AutoClear(true)
-        .HideCompleted(true)
-        .Start(ctx => images = new(GetImagePaths(options.From, 0, options.Depth, ctx)));
+    // Start copy images
+    var duplicates = CopyImages(images, options.To, options.Structure);
 
+    if (duplicates.Count > 0)
+    {
+      // Ask what should happen with duplicates.
       AnsiConsole.Write(
-        new Rule($"Metadata loaded. [yellow]{images.Count}[/] files to distribute!")
+        new Rule($"[green]There are [yellow]{duplicates.Count}[/] duplicate files![/]")
         {
           Style = Style.Parse("green"),
         }
       );
 
-      if (images.Count == 0)
-      {
-        AnsiConsole.MarkupLine("No images found. Finished!");
-        return 0;
-      }
+      AnsiConsole.WriteLine();
 
-      // Cast (path, date) to (path) queue.
-      var imagePaths = new Queue<string>(
-        images.Cast<(string, DateTime)>().Select(image => image.Item1)
+      var duplicatesOption = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+          .Title("[yellow]How would you like to proceed with the duplicate files?[/]")
+          .AddChoices(["Skip", "Overwrite", "Add \"_copy\" to filename"])
       );
 
-      // Start copy images
-      var duplicates = CopyImages(images, options.To, options.Structure);
-
-      if (duplicates.Count > 0)
+      switch (duplicatesOption)
       {
-        // Ask what should happen with duplicates.
-        AnsiConsole.Write(
-          new Rule($"[green]There are [yellow]{duplicates.Count}[/] duplicate files![/]")
-          {
-            Style = Style.Parse("green"),
-          }
-        );
-
-        AnsiConsole.WriteLine();
-
-        var duplicatesOption = AnsiConsole.Prompt(
-          new SelectionPrompt<string>()
-            .Title("[yellow]How would you like to proceed with the duplicate files?[/]")
-            .AddChoices(["Skip", "Overwrite", "Add \"_copy\" to filename"])
-        );
-
-        switch (duplicatesOption)
-        {
-          case "Skip":
-            break;
-          case "Overwrite":
-            CopyImages(duplicates, options.To, options.Structure, true);
-            break;
-          default:
-            CopyImages(duplicates, options.To, options.Structure, false, "_copy");
-            break;
-        }
+        case "Skip":
+          break;
+        case "Overwrite":
+          CopyImages(duplicates, options.To, options.Structure, true);
+          break;
+        default:
+          CopyImages(duplicates, options.To, options.Structure, false, "_copy");
+          break;
       }
-
-      // Remove images in source.
-      if (options.Remove)
-      {
-        AnsiConsole.Write(
-          new Rule("[yellow]Removing files in source directory![/]")
-          {
-            Style = Style.Parse("yellow"),
-          }
-        );
-        AnsiConsole
-          .Progress()
-          .Columns(
-            [
-              new TaskDescriptionColumn(),
-              new ProgressBarColumn(),
-              new PercentageColumn(),
-              new RemainingTimeColumn(),
-              new ElapsedTimeColumn(),
-            ]
-          )
-          .Start(ctx =>
-          {
-            var task = ctx.AddTask("Removing files...", maxValue: imagePaths.Count);
-
-            do
-            {
-              var path = imagePaths.Dequeue();
-
-              try
-              {
-                File.Delete(path);
-              }
-              catch (IOException)
-              {
-                AnsiConsole.MarkupLine($"[yellow]Image is in use:[/] [gray]{path}[/]");
-              }
-              catch (UnauthorizedAccessException)
-              {
-                AnsiConsole.MarkupLine(
-                  $"[yellow]Missing permission to delete image:[/] [gray]{path}[/]"
-                );
-              }
-
-              task.Increment(1);
-            } while (imagePaths.Count > 0);
-          });
-      }
-
-      AnsiConsole.Write(
-        new Rule("[blue]All jobs done successfully![/]") { Style = Style.Parse("blue") }
-      );
-
-      return 0;
     }
 
-    private static Queue<(string, DateTime)> CopyImages(
-      Queue<(string, DateTime)> images,
-      string to,
-      string structure,
-      bool overwrite = false,
-      string? filenameAppend = null
-    )
+    // Remove images in source.
+    if (options.Remove)
     {
-      var duplicates = new Queue<(string, DateTime)>();
-
+      AnsiConsole.Write(
+        new Rule("[yellow]Removing files in source directory![/]") { Style = Style.Parse("yellow") }
+      );
       AnsiConsole
         .Progress()
         .Columns(
@@ -161,115 +102,170 @@ namespace Distribute
         )
         .Start(ctx =>
         {
-          var task = ctx.AddTask("Copying files... ", maxValue: images.Count);
+          var task = ctx.AddTask("Removing files...", maxValue: imagePaths.Count);
 
           do
           {
-            var image = images.Dequeue();
-            var directory = to + @"\" + image.Item2.ToString(structure);
-            var path = directory + Path.GetFileName(image.Item1);
-            if (filenameAppend != null)
-            {
-              var split = path.Split('.');
-              split[0] += filenameAppend;
-              path = split[0] + "." + split[1];
-            }
-
-            if (!Directory.Exists(directory))
-            {
-              Directory.CreateDirectory(directory);
-            }
+            var path = imagePaths.Dequeue();
 
             try
             {
-              File.Copy(image.Item1, path, overwrite);
+              File.Delete(path);
             }
             catch (IOException)
             {
-              duplicates.Enqueue(image);
+              AnsiConsole.MarkupLine($"[yellow]Image is in use:[/] [gray]{path}[/]");
             }
             catch (UnauthorizedAccessException)
             {
               AnsiConsole.MarkupLine(
-                $"[yellow]Missing permission copy image:[/] [gray]{image.Item1}[/][white] to [/][grey]{path}[/]"
+                $"[yellow]Missing permission to delete image:[/] [gray]{path}[/]"
               );
             }
 
             task.Increment(1);
-          } while (images.Count > 0);
+          } while (imagePaths.Count > 0);
         });
-
-      return duplicates;
     }
 
-    private static List<(string, DateTime)> GetImagePaths(
-      string directory,
-      uint depth,
-      int maxDepth,
-      ProgressContext ctx
-    )
-    {
-      string[] files = Directory.GetFiles(directory);
+    AnsiConsole.Write(
+      new Rule("[blue]All jobs done successfully![/]") { Style = Style.Parse("blue") }
+    );
 
-      var task = ctx.AddTask(
-        $"[grey][white]Loading images...         [/] {directory}[/]",
-        maxValue: files.Length + 1
-      );
+    return 0;
+  }
 
-      List<(string, DateTime)> images = new(files.Length);
+  private static Queue<(string, DateTime)> CopyImages(
+    Queue<(string, DateTime)> images,
+    string to,
+    string structure,
+    bool overwrite = false,
+    string? filenameAppend = null
+  )
+  {
+    var duplicates = new Queue<(string, DateTime)>();
 
-      foreach (string image in files)
+    AnsiConsole
+      .Progress()
+      .Columns(
+        [
+          new TaskDescriptionColumn(),
+          new ProgressBarColumn(),
+          new PercentageColumn(),
+          new RemainingTimeColumn(),
+          new ElapsedTimeColumn(),
+        ]
+      )
+      .Start(ctx =>
       {
-        ExifSubIfdDirectory? metadata = null;
+        var task = ctx.AddTask("Copying files... ", maxValue: images.Count);
 
-        try
+        do
         {
-          metadata = ImageMetadataReader
-            .ReadMetadata(image)
-            .OfType<ExifSubIfdDirectory>()
-            .FirstOrDefault()!;
-        }
-        catch (ImageProcessingException)
-        {
-          AnsiConsole.MarkupLine($"[yellow]Could not process file:[/] [gray]{image}[/]");
-        }
+          var image = images.Dequeue();
+          var directory = to + @"\" + image.Item2.ToString(structure);
+          var path = directory + Path.GetFileName(image.Item1);
+          if (filenameAppend != null)
+          {
+            var split = path.Split('.');
+            split[0] += filenameAppend;
+            path = split[0] + "." + split[1];
+          }
 
-        task.Increment(1);
+          if (!Directory.Exists(directory))
+          {
+            Directory.CreateDirectory(directory);
+          }
 
-        if (metadata == null)
-        {
-          continue;
-        }
-
-        if (metadata.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out DateTime datetime))
-        {
-          images.Add((image, datetime));
-        }
-      }
-
-      var subdirectories = Directory.GetDirectories(directory);
-
-      if (subdirectories.Length == 0)
-      {
-        task.Value = task.MaxValue;
-        return images;
-      }
-
-      task.Description = $"[grey][white]Loading sub directories...[/] {directory}[/]";
-      task.Value = 0;
-      task.MaxValue = subdirectories.Length;
-
-      if (depth < maxDepth)
-      {
-        foreach (string subdirectory in subdirectories)
-        {
-          images.AddRange(GetImagePaths(subdirectory, depth + 1, maxDepth, ctx));
+          try
+          {
+            File.Copy(image.Item1, path, overwrite);
+          }
+          catch (IOException)
+          {
+            duplicates.Enqueue(image);
+          }
+          catch (UnauthorizedAccessException)
+          {
+            AnsiConsole.MarkupLine(
+              $"[yellow]Missing permission copy image:[/] [gray]{image.Item1}[/][white] to [/][grey]{path}[/]"
+            );
+          }
 
           task.Increment(1);
-        }
+        } while (images.Count > 0);
+      });
+
+    return duplicates;
+  }
+
+  private static List<(string, DateTime)> GetImagePaths(
+    string directory,
+    uint depth,
+    int maxDepth,
+    ProgressContext ctx
+  )
+  {
+    string[] files = Directory.GetFiles(directory);
+
+    var task = ctx.AddTask(
+      $"[grey][white]Loading images...         [/] {directory}[/]",
+      maxValue: files.Length + 1
+    );
+
+    List<(string, DateTime)> images = new(files.Length);
+
+    foreach (string image in files)
+    {
+      ExifSubIfdDirectory? metadata = null;
+
+      try
+      {
+        metadata = ImageMetadataReader
+          .ReadMetadata(image)
+          .OfType<ExifSubIfdDirectory>()
+          .FirstOrDefault()!;
+      }
+      catch (ImageProcessingException)
+      {
+        AnsiConsole.MarkupLine($"[yellow]Could not process file:[/] [gray]{image}[/]");
       }
 
+      task.Increment(1);
+
+      if (metadata == null)
+      {
+        continue;
+      }
+
+      if (metadata.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out DateTime datetime))
+      {
+        images.Add((image, datetime));
+      }
+    }
+
+    var subdirectories = Directory.GetDirectories(directory);
+
+    if (subdirectories.Length == 0)
+    {
+      task.Value = task.MaxValue;
       return images;
     }
+
+    task.Description = $"[grey][white]Loading sub directories...[/] {directory}[/]";
+    task.Value = 0;
+    task.MaxValue = subdirectories.Length;
+
+    if (depth < maxDepth)
+    {
+      foreach (string subdirectory in subdirectories)
+      {
+        images.AddRange(GetImagePaths(subdirectory, depth + 1, maxDepth, ctx));
+
+        task.Increment(1);
+      }
+    }
+
+    return images;
   }
 }
--- Options.cs
+++ Options.cs
@@ -1,84 +1,51 @@
-﻿namespace Distribute
+﻿using System.ComponentModel;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Distribute;
+
+public class Options : CommandSettings
 {
-  using System.ComponentModel;
-  using Spectre.Console;
-  using Spectre.Console.Cli;
+  [Description("The source directory the files should be distributed from.")]
+  [CommandArgument(0, "[From]")]
+  [DefaultValue("./")]
+  public string From { get; init; } = null!;
 
-  public class Options : CommandSettings
+  [Description("The directory the files should be distributed to.")]
+  [CommandArgument(0, "[To]")]
+  [DefaultValue("./")]
+  public string To { get; init; } = null!;
+
+  [Description(
+    "The folder structure the files should be sorted into.\nSee https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings for more information."
+  )]
+  [CommandOption("-s|--structure")]
+  [DefaultValue("yyyy/MM/")]
+  public string Structure { get; init; } = null!;
+
+  [Description("The maximum search depth for files in the source directory.")]
+  [CommandOption("-d|--depth")]
+  [DefaultValue(5)]
+  public int Depth { get; init; }
+
+  [Description(
+    "Whether the distributed and copied files should be deleted in the source directory."
+  )]
+  [CommandOption("-r|--remove")]
+  public bool Remove { get; init; }
+
+  public override ValidationResult Validate()
   {
-    public Options(string from, string to, string structure, int depth, bool remove)
+    if (Depth < 1)
     {
-      From = from == "The current directory." ? Directory.GetCurrentDirectory() : from;
-      To = to == "The current directory." ? Directory.GetCurrentDirectory() : to;
-      Structure = structure ?? @"yyyy\\MM\\";
-      Depth = depth;
-      Remove = remove;
+      return ValidationResult.Error("Option [-d|--depth] must be at least 1.");
     }
 
-    [Description("The source directory the files should be distributed from.")]
-    [CommandOption("-f|--from")]
-    [DefaultValue("The current directory.")]
-    public string From { get; private set; }
-
-    [Description("The directory the files should be distributed to.")]
-    [CommandOption("-t|--to")]
-    [DefaultValue("The current directory.")]
-    public string To { get; private set; }
-
-    [Description(
-      "The folder structure the files should be sorted into.\nSee https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings for more information."
-    )]
-    [CommandOption("-s|--structure")]
-    [DefaultValue(@"yyyy\\MM\\")]
-    public string Structure { get; }
-
-    [Description("The maximum search depth for files in the source directory.")]
-    [CommandOption("-d|--depth")]
-    [DefaultValue(5)]
-    public int Depth { get; } = 5;
-
-    [Description(
-      "Whether the distributed and copied files should be deleted in the source directory."
-    )]
-    [CommandOption("-r|--remove")]
-    public bool Remove { get; }
-
-    public override ValidationResult Validate()
+    if (From == Directory.GetCurrentDirectory() && To == Directory.GetCurrentDirectory())
     {
-      if (Depth < 1)
-      {
-        return ValidationResult.Error("Option [-d|--depth] must be at least 1.");
-      }
-
-      if (
-        From == Directory.GetCurrentDirectory()
-        && To == Directory.GetCurrentDirectory()
-      )
-      {
-        return ValidationResult.Error(
-          "Either option [-f|--from] or [-t|--to] has to be specified."
-        );
-      }
-
-      try
-      {
-        From = Path.GetFullPath(From);
-      }
-      catch
-      {
-        return ValidationResult.Error("The option [-f|--from] is an invalid path.");
-      }
-
-      try
-      {
-        To = Path.GetFullPath(To);
-      }
-      catch
-      {
-        return ValidationResult.Error("The option [-t|--to] is an invalid path.");
-      }
-
-      return ValidationResult.Success();
+      return ValidationResult.Error("Either option [-f|--from] or [-t|--to] has to be specified.");
     }
+
+    return ValidationResult.Success();
   }
 }
